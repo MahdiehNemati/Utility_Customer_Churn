dfd1ce4095bb6b3fd4b627cca29618a773679a34
MahdiehNemati <83155382+MahdiehNemati@users.noreply.github.com>
2021-08-04 16:10:03 +0930

    plot distributions

--- loading_data.py
+++ loading_data.py
@@ -41,13 +41,11 @@ def plot_stacked_bars(dataframe, title_, size_=(18, 10), rot_=0, legend_="upper 
     # Labels
     plt.ylabel("Company base (%)")
     plt.show()
-
-
 def annotate_stacked_bars(ax, pad=0.99, colour="white", textsize=13):
     """
     Add value annotations to the bars
     """
-    # Iterate over the plotted rectanges/bars
+    # Iterate over the plotted rectangles/bars
     for p in ax.patches:
         # Calculate annotation
         value = str(round(p.get_height(), 1))
@@ -56,7 +54,6 @@ def annotate_stacked_bars(ax, pad=0.99, colour="white", textsize=13):
             continue
         ax.annotate(value, ((p.get_x() + p.get_width() / 2) * pad - 0.05, (p.get_y() + p.get_height() / 2) * pad),
                     color=colour, size=textsize, )
-
 
 churn_total = churn.groupby(churn["churn"]).count()
 churn_percentage = churn_total / churn_total.sum() * 100
@@ -119,3 +116,139 @@ def plot_distribution(dataframe, column, ax, bins_=50):
     # Change the x-axis to plain style
     ax.ticklabel_format(style='plain', axis='x')
 
+fig, axs = plt.subplots(nrows=4, figsize=(18, 25))
+# Plot histogram
+plot_distribution(consumption, "cons_12m", axs[0])
+# Note that the gas consumption must have gas contract
+plot_distribution(consumption[consumption["has_gas"] == "t"], "cons_gas_12m", axs[1])
+plot_distribution(consumption, "cons_last_month", axs[2])
+plot_distribution(consumption, "imp_cons", axs[3])
+
+fig, axs = plt.subplots(nrows=4, figsize=(18, 25))
+# Plot histogram
+sns.boxplot(consumption["cons_12m"], ax=axs[0])
+sns.boxplot(consumption[consumption["has_gas"] == "t"]["cons_gas_12m"], ax=axs[1])
+sns.boxplot(consumption["cons_last_month"], ax=axs[2])
+sns.boxplot(consumption["imp_cons"], ax=axs[3])
+# Remove scientific notation
+for ax in axs:
+    ax.ticklabel_format(style='plain', axis='x')
+# Set x-axis limit
+axs[0].set_xlim(-200000, 2000000)
+axs[1].set_xlim(-200000, 2000000)
+axs[2].set_xlim(-20000, 100000)
+plt.show()
+
+dates = train[["id", "date_activ", "date_end", "date_modif_prod", "date_renewal", "churn"]].copy()
+
+dates["date_activ"] = pd.to_datetime(dates["date_activ"], format='%Y-%m-%d')
+dates["date_end"] = pd.to_datetime(dates["date_end"], format='%Y-%m-%d')
+dates["date_modif_prod"] = pd.to_datetime(dates["date_modif_prod"], format='%Y-%m-%d')
+dates["date_renewal"] = pd.to_datetime(dates["date_renewal"], format='%Y-%m-%d')
+
+
+def plot_dates(dataframe, column, fontsize_=12):
+    """
+    Plot monthly churn and retention distribution
+    """
+    # Group by month
+    temp = dataframe[[column,
+                      "churn",
+                      "id"]].set_index(column).groupby([pd.Grouper(freq='M'), "churn"]).count().unstack(level=1)
+    # Plot
+    ax = temp.plot(kind="bar", stacked=True, figsize=(18, 10), rot=0)
+    # Change x-axis labels to months
+    ax.set_xticklabels(map(lambda x: line_format(x), temp.index))
+    # Change xlabel size
+    plt.xticks(fontsize=fontsize_)
+    # Rename y-axis
+    plt.ylabel("Number of companies")
+    # Rename legend
+    plt.legend(["Retention", "Churn"], loc="upper right")
+    plt.show()
+
+
+def line_format(label):
+    """
+    Convert time label to the format of pandas line plot
+    """
+    month = label.month_name()[:1]
+    if label.month_name() == "January":
+        month += f'\n{label.year}'
+    return month
+
+
+plot_dates(dates, "date_activ", fontsize_=8)
+
+plot_dates(dates, "date_end")
+
+plot_dates(dates, "date_modif_prod", fontsize_=8)
+
+plot_dates(dates, "date_renewal")
+
+forecast = train[["id", "forecast_base_bill_ele", "forecast_base_bill_year",
+                  "forecast_bill_12m", "forecast_cons", "forecast_cons_12m",
+                  "forecast_cons_year", "forecast_discount_energy", "forecast_meter_rent_12m",
+                  "forecast_price_energy_p1", "forecast_price_energy_p2",
+                  "forecast_price_pow_p1", "churn"]]
+
+fig, axs = plt.subplots(nrows=11, figsize=(18, 50))
+# Plot histogram
+plot_distribution(train, "forecast_base_bill_ele", axs[0])
+plot_distribution(train, "forecast_base_bill_year", axs[1])
+plot_distribution(train, "forecast_bill_12m", axs[2])
+plot_distribution(train, "forecast_cons", axs[3])
+plot_distribution(train, "forecast_cons_12m", axs[4])
+plot_distribution(train, "forecast_cons_year", axs[5])
+plot_distribution(train, "forecast_discount_energy", axs[6])
+plot_distribution(train, "forecast_meter_rent_12m", axs[7])
+plot_distribution(train, "forecast_price_energy_p1", axs[8])
+plot_distribution(train, "forecast_price_energy_p2", axs[9])
+plot_distribution(train, "forecast_price_pow_p1", axs[10])
+
+contract_type = train[["id", "has_gas", "churn"]]
+
+contract = contract_type.groupby([contract_type["churn"],
+                                  contract_type["has_gas"]])["id"].count().unstack(level=0)
+
+contract_percentage = (contract.div(contract.sum(axis=1), axis=0) * 100).sort_values(by=[1], ascending=False)
+
+plot_stacked_bars(contract_percentage, "Contract type (with gas)")
+
+margin = train[["id", "margin_gross_pow_ele", "margin_net_pow_ele", "net_margin"]]
+
+fig, axs = plt.subplots(nrows=3, figsize=(18, 20))
+# Plot histogram
+sns.boxplot(margin["margin_gross_pow_ele"], ax=axs[0])
+sns.boxplot(margin["margin_net_pow_ele"], ax=axs[1])
+sns.boxplot(margin["net_margin"], ax=axs[2])
+# Remove scientific notation
+axs[0].ticklabel_format(style='plain', axis='x')
+axs[1].ticklabel_format(style='plain', axis='x')
+axs[2].ticklabel_format(style='plain', axis='x')
+plt.show()
+
+
+power = train[["id","pow_max", "churn"]].fillna(0)
+
+
+fig, axs = plt.subplots(nrows=1, figsize=(18,10))
+plot_distribution(power, "pow_max", axs)
+
+
+others = train[["id","nb_prod_act","num_years_antig", "origin_up", "churn"]]
+
+
+products = others.groupby([others["nb_prod_act"],others["churn"]])["id"].count().unstack(level=1)
+products_percentage = (products.div(products.sum(axis=1), axis=0)*100).sort_values(by=[1], ascending=False)
+plot_stacked_bars(products_percentage, "Number of products")
+
+
+years_antig = others.groupby([others["num_years_antig"],others["churn"]])["id"].count().unstack(level=1)
+years_antig_percentage = (years_antig.div(years_antig.sum(axis=1), axis=0)*100)
+plot_stacked_bars(years_antig_percentage, "Number years")
+
+
+origin = others.groupby([others["origin_up"],others["churn"]])["id"].count().unstack(level=1)
+origin_percentage = (origin.div(origin.sum(axis=1), axis=0)*100)
+plot_stacked_bars(origin_percentage, "Origin contract/offer")
